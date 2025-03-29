cc3b987952f8d3dc341d1b78d1d191704021dc50
jun6837 <jun6837@naver.com>
2025-03-29 14:52:45 +0900

    whiel문

---
+++ Practice/src/chap2/Bread.java
@@ -0,0 +1,14 @@
+package chap2;
+
+public class Bread {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int bread = 25;
+     int man = 4;
+     
+     System.out.println("각자의 몫: "+ bread/man);
+     System.out.println("남은 빵의 개수: "+ bread%man);
+	}
+
+}
